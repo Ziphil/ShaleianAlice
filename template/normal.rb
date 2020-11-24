c3205bcb3effe4b70b3dedbd53c5194b5f9ae7f2
Ziphil <yfe23457@nifty.com>
2020-11-24 19:18:49 +0900

    変更

--- template/normal.rb
+++ template/normal.rb
@@ -9,9 +9,27 @@ converter.add(["slide"], ["root"]) do |element, _, number|
     this << Tag.build("div", "slide") do |this|
       this << Tag.new("div", "header")
       this << Tag.new("div", "footer")
+      this << Tag.build("div", "logo") do |this|
+        this << Tag.build("image") do |this|
+          this["src"] = "../asset/logo.svg"
+        end
+      end
       this << Tag.build("div", "content") do |this|
         this << apply(element, "slide", count)
       end
+    end
+  end
+  next this
+end
+
+converter.add(["sh", "en"], ["slide"]) do |element, _, count|
+  this = ""
+  duplication = (element.name == "sh") ? 2 : 1
+  duplication.times do
+    this << Tag.build("div", "text") do |this|
+      this.set_range(element, count)
+      this["class"] << " " + element.name
+      this << apply(element, "slide", count)
     end
   end
   next this
