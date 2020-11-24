ab5490eeaba529111385ccd4fbf857f1c6dc458f
Ziphil <yfe23457@nifty.com>
2020-11-24 19:34:46 +0900

    スライド番号を表示するよう変更

--- template/normal.rb
+++ template/normal.rb
@@ -3,12 +3,15 @@
 
 converter.add(["slide"], ["root"]) do |element, _, number|
   this = ""
-  slide_number = element.attribute("number").to_s.to_i
   Tag.repeat(element) do |count|
     variables[:slide_count] = variables[:slide_count].to_i + 1
+    slide_number = variables[:slide_count].to_s.to_i
     this << Tag.build("div", "slide") do |this|
       this << Tag.new("div", "header")
       this << Tag.new("div", "footer")
+      this << Tag.build("div", "number") do |this|
+        this << slide_number.to_s
+      end
       this << Tag.build("div", "logo") do |this|
         this << Tag.build("image") do |this|
           this["src"] = "../asset/logo.svg"
